86b26e607d9eb7412d4e47cb06696ababc7dc7b8
AJ <arjames1618@gmail.com>
2021-03-08 21:59:41 +0000

    added individual 'pixel to character' conversion

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,54 @@
 using namespace cv;
 using namespace std;
 
+Mat crop_image(Mat image){
+    Size size = image.size();
+    Rect crop_region(0, 0, size.width - (size.width % 8), size.height - (size.height % 14));
+    // Rect crop_region(0, 0, 251, 251);
+    return image(crop_region);
+}
+
+void modify_image(Mat image){
+    Size size = image.size();
+    int x = 0;
+    int y = 0;
+    int recordX = 0;
+    int recordY = 0;
+
+    int b = 0; int g = 0; int r = 0;
+
+    for (int k = 0; k < (size.width / 8) * (size.height / 14); k++)
+    {
+        for (int j = 0; j < 14; j++){
+            for (int i = 0; i < 8; i++){            
+                Vec3b color = image.at<Vec3b>(Point(x,y));
+                //cout << "x: " + to_string(x) + " y: " + to_string(y) + "[" + to_string(color[2]) + ", " +to_string(color[1])+ ", " + to_string(color[0]) + "]" << endl;
+
+                b += color[0];
+                g += color[1];
+                r += color[2];
+
+                x++;
+                if (x == recordX + 8) x -= 8;
+            }
+            y++;
+            if (y == recordY + 14) y -= 14;
+        }
+        x += 8;
+        recordX += 8;
+        if (x == size.width){
+            x = 0;
+            recordX = 0;
+            y += 14;
+            recordY += 14;
+        }
+
+        cout << "x: " + to_string(x) + " y: " + to_string(y) << endl;
+    }
+    
+    cout << to_string(size.width) + " x " + to_string(size.height) << endl;
+}
+
 int main( int argc, char** argv )
 {
     if( argc != 2)
@@ -14,7 +62,7 @@ int main( int argc, char** argv )
      return -1;
     }
 
-    Mat image;
+    Mat image, image_modified;
     image = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
 
     if(! image.data )                              // Check for invalid input
@@ -23,9 +71,30 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
-    imshow( "Display window", image );                   // Show our image inside it.
+    image = crop_image(image);
 
-    waitKey(0);                                          // Wait for a keystroke in the window
+    // Vec3b color = image.at<Vec3b>(Point(150,150));
+
+    // int b, g, r;
+    // b = color[0];
+    // g = color[1];
+    // r = color[2];
+
+    // cout << "[" + std::to_string(r) + ", " + std::to_string(g)+ ", " + std::to_string(b) + "]" << endl;
+
+    modify_image(image);
+
+    
+
+
+
+
+
+    namedWindow( "original image", WINDOW_AUTOSIZE );
+    imshow( "original image", image );
+    // namedWindow( "modified image", WINDOW_AUTOSIZE );
+    // imshow( "modified image", image_modified );
+
+    // waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
 }
